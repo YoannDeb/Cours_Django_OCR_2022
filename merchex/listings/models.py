5ca11e76fd87c8effb2fd8f21c7c7e1f13e95700
Yoann <yoann.deblois@gmail.com>
2023-01-23 21:15:53 +0100

    Func: Setup of custom users (notably username is the mail)

--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -1,3 +1,60 @@
-from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from django.db import models, transaction
+from django.conf import settings
 
-# Create your models here.
+
+class CustomUserManager(BaseUserManager):
+    """
+    Custom user manager is necessary for custom user with custom fields (notably email as username) to work.
+    """
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError('User must have an email address')
+        with transaction.atomic():
+            user = self.model(email=self.normalize_email(email), **extra_fields)
+            user.set_password(password)
+            user.save(using=self._db)
+            return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_superuser', True)
+        return self._create_user(email, password, **extra_fields)
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    """
+    Custom user, notably for email as username.
+    Declared in settings.py
+    """
+    email = models.EmailField(
+        max_length=255, unique=True, verbose_name='email address',
+        error_messages={'unique':'A user with this email already exists.'})
+    first_name = models.CharField(max_length=50, blank=True)
+    last_name = models.CharField(max_length=50, blank=True)
+    is_active = models.BooleanField(default=True)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} - {self.email}"
+
+    @property
+    def is_staff(self):
+        """Is the user a member of staff?"""
+        return self.is_superuser
+
+    class Meta:
+        verbose_name = "User"
+        verbose_name_plural = "Users"
+
+"""
+class Band(models.Model):
+    name = models.fields.CharField(max_length=100)
+"""
--- merchex/merchex/settings.py
+++ merchex/merchex/settings.py
@@ -128,6 +128,8 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+AUTH_USER_MODEL = 'listings.CustomUser'
+
 INTERNAL_IPS = ['127.0.0.1']
 
 STATICFILES_DIRS = [
