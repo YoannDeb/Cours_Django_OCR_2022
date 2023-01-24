207fffa01cc11951b0af24b06004784a6ddf3f7b
Yoann <yoann.deblois@gmail.com>
2023-01-24 18:09:06 +0100

    Func: Display of bands list in hello view

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from listings.models import Band
 
 def hello(request):
-    return HttpResponse('<h1>Hello Django!</h1>')
-
+    bands = Band.objects.all()
+    return HttpResponse(f"""
+            <h1>Hello Django !</h1>
+            <p>Mes groupes préférés sont :<p>
+            <ul>
+                <li>{bands[0].name}</li>
+                <li>{bands[1].name}</li>
+                <li>{bands[2].name}</li>
+            </ul>
+    """)
 def about(request):
     return HttpResponse('<h1>A propos</h1> <p>Nous adorons le merch !</p>')
