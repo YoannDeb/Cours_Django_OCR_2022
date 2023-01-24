7c97b5cb9fae1d3bca7128071382fb61886ce99b
Yoann <yoann.deblois@gmail.com>
2023-01-24 18:22:11 +0100

    Func: Creation of model Listing

--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -57,3 +57,6 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
 class Band(models.Model):
     name = models.fields.CharField(max_length=100)
 
+
+class Listing(models.Model):
+    title = models.fields.CharField(max_length=100)
