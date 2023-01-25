c40385d7c1d6eba13a9ba1263c1e349870655deb
Yoann <yoann.deblois@gmail.com>
2023-01-25 16:47:31 +0100

    Func: Display of bands and listings in html lists through context in hello view

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -5,7 +5,11 @@ from listings.models import Band, Listing
 def hello(request):
     bands = Band.objects.all()
     listings = Listing.objects.all()
-    return render(request, 'listings/hello.html')
+    context = {
+        'bands': bands,
+        'listings': listings,
+    }
+    return render(request, 'listings/hello.html', context)
 
 def about(request):
     return HttpResponse('<h1>A propos</h1> <p>Nous adorons le merch !</p>')
