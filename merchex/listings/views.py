416401552c716745af7685e3b1a41051803741de
Yoann <yoann.deblois@gmail.com>
2023-01-25 16:18:54 +0100

    Func: creation of template and template display in hello view

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -5,21 +5,7 @@ from listings.models import Band, Listing
 def hello(request):
     bands = Band.objects.all()
     listings = Listing.objects.all()
-    return HttpResponse(f"""
-            <h1>Hello Django !</h1>
-            <p>Mes groupes préférés sont :<p>
-            <ul>
-                <li>{bands[0].name}</li>
-                <li>{bands[1].name}</li>
-                <li>{bands[2].name}</li>
-            </ul>
-            <p>Les annonces :<p>
-            <ul>
-                <li>{listings[0].title}</li>
-                <li>{listings[1].title}</li>
-                <li>{listings[2].title}</li>
-                <li>{listings[3].title}</li>
-            </ul>
-    """)
+    return render(request, 'listings/hello.html')
+
 def about(request):
     return HttpResponse('<h1>A propos</h1> <p>Nous adorons le merch !</p>')
