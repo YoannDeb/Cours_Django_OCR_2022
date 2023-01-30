323e6c0228020b70815e62f8c9a76aa2744cf246
Yoann <yoann.deblois@gmail.com>
2023-01-30 18:12:54 +0100

    Func: links between band list and band detail and vice versa

--- merchex/merchex/urls.py
+++ merchex/merchex/urls.py
@@ -21,8 +21,8 @@ from listings import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('bands/', views.band_list),
-    path('bands/<int:band_id>', views.band_detail),
+    path('bands/', views.band_list, name='band-list'),
+    path('bands/<int:band_id>', views.band_detail, name='band-detail'),
     path('listings/', views.listings),
     path('about-us/', views.about_us),
     path('contact/', views.contact)
